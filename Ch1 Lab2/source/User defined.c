3bc9dc82003745601f6eee54f2d0c642a21d4bf3
unknown <t109360252@ntut.org.tw>
2020-09-25 21:37:53 +0800

    C++

---
+++ Ch1 Lab2/source/User defined.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+
+
+
+
+
+
+int User_defined_print()
+{
+	printf("welcome to C!\n");
+	system("pause");
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Ch1 Lab2/source/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<User defined.h>
+int main(void)
+{
+	User_defined_print();
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
